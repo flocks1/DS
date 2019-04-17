b0f7598581c043589a0cfcd07a61964a64d34b84
BrittonWinterrose <BrittonWinterrose@users.noreply.github.com>
2019-04-17 15:40:19 -0700

    Moved 'w/o AUC' code into toplevel directory

--- Bert-Multi-Label-Text-Classification/pybert/train/metrics.py
+++ Bert-Multi-Label-Text-Classification/pybert/train/metrics.py
@@ -291,13 +291,13 @@ class MultiLabelReport(Metric):
         self.y_prob = logits.sigmoid().data.cpu().detach().numpy()
         self.y_true = target.cpu().numpy()
 
-    def value(self):
-        '''
-        计算指标得分
-        '''
-        for i, label in self.id2label.items():
-            auc = roc_auc_score(y_score=self.y_prob[:, i], y_true=self.y_true[:, i])
-            print(f"label:{label} - auc: {auc:.4f}")
+    # def value(self):
+    #     '''
+    #     计算指标得分
+    #     '''
+    #     for i, label in self.id2label.items():
+    #         auc = roc_auc_score(y_score=self.y_prob[:, i], y_true=self.y_true[:, i])
+    #         print(f"label:{label} - auc: {auc:.4f}")
 
     def name(self):
         return "multilabel_report"
